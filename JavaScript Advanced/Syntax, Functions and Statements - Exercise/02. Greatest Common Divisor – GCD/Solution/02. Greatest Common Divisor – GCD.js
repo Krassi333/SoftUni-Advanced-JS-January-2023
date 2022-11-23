e52a3bdf7be7fba7a6c8a0fb385cd52dabbd0dc4
Krassi333 <krassi.pln@gmail.com>
2022-11-23 23:41:13 +0200

    Add new task

---
+++ JavaScript Advanced/Syntax, Functions and Statements - Exercise/02. Greatest Common Divisor – GCD/Solution/02. Greatest Common Divisor – GCD.js
@@ -0,0 +1,9 @@
+function greatestCommonDivisorGCD(num1, num2) {
+    for (let i = Math.max(num1, num2); i >= 0; i--) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            console.log(i);
+            return;
+        }
+    }
+}
+greatestCommonDivisorGCD(2154, 458)
