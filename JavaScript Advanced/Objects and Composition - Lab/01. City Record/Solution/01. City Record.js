5cd050dfe403b1ea029dda450e2e5057337a972f
Krassi333 <krassi.pln@gmail.com>
2023-01-09 10:46:36 +0200

    Add new solution

---
+++ JavaScript Advanced/Objects and Composition - Lab/01. City Record/Solution/01. City Record.js
@@ -0,0 +1,14 @@
+function cityRecord(name,population,treasury) {
+   
+    return {
+        name,
+        population: population,
+        treasury: treasury
+    }
+}
+
+console.log(cityRecord(['Tortuga',
+
+    7000,
+
+    15000])); 
