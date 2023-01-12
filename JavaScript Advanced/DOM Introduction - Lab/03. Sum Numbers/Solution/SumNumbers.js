7cab7e9184d33027ae7bdf50c6742dc453a75c14
Krassi333 <krassi.pln@gmail.com>
2023-01-12 17:19:15 +0200

    Add task solution

---
+++ JavaScript Advanced/DOM Introduction - Lab/03. Sum Numbers/Solution/SumNumbers.js
@@ -0,0 +1,7 @@
+function calc() {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let num = Number(num1) + Number(num2);
+    document.getElementById('sum').value = num;
+
+}
