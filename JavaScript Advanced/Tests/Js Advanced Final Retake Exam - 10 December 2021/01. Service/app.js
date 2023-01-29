7a95ba0527eafa79ce05aaaf26beeb9fb689bccb
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-01-29 15:00:35 +0200

    Update the solution

--- JavaScript Advanced/Tests/Js Advanced Final Retake Exam - 10 December 2021/01. Service/app.js
+++ JavaScript Advanced/Tests/Js Advanced Final Retake Exam - 10 December 2021/01. Service/app.js
@@ -2,16 +2,15 @@ window.addEventListener('load', solve);
 
 function solve() {
     let sendBtn = document.querySelector('#right form button');
+    let descriptionField = document.getElementById('description');
+    let nameField = document.getElementById('client-name');
+    let phoneField = document.getElementById('client-phone');
     let receivedSection = document.getElementById('received-orders');
     let completeSection = document.getElementById('completed-orders');
     let clearBtn = document.getElementsByClassName('clear-btn');
 
     sendBtn.addEventListener('click', (event) => {
         event.preventDefault();
-
-        let descriptionField = document.getElementById('description');
-        let nameField = document.getElementById('client-name');
-        let phoneField = document.getElementById('client-phone');
 
         let info = {
             description: descriptionField.value,
@@ -20,30 +19,27 @@ function solve() {
         }
 
         if (validate(info)) {
-            descriptionField.value = '';
-            nameField.value = '';
-            phoneField.value = '';
 
             let div = document.createElement('div');
             div.setAttribute('class', 'container');
-            receivedSection.append(div);
+            receivedSection.appendChild(div);
 
             let h2 = document.createElement('h2');
             h2.innerText = `Product type for repair: ${document.getElementById('type-product').value}`;
-            div.append(h2);
+            div.appendChild(h2);
 
             let h3 = document.createElement('h3');
             h3.innerText = `Client information: ${info.name}, ${info.phone}`;
-            div.append(h3);
+            div.appendChild(h3);
 
             let h4 = document.createElement('h4');
             h4.innerText = `Description of the problem: ${info.description}`;
-            div.append(h4);
+            div.appendChild(h4);
 
             let startBtn = document.createElement('button');
             startBtn.innerText = 'Start repair';
             startBtn.setAttribute('class', 'start-btn');
-            div.append(startBtn);
+            div.appendChild(startBtn);
             startBtn.addEventListener('click', () => {
                 startBtn.disabled = true;
                 finishBtn.disabled = false;
@@ -52,22 +48,23 @@ function solve() {
             let finishBtn = document.createElement('button');
             finishBtn.innerText = 'Finish repair';
             finishBtn.setAttribute('class', 'finish-btn');
-            div.append(finishBtn);
+            finishBtn.disabled = true;
+            div.appendChild(finishBtn);
             finishBtn.addEventListener('click', () => {
                 div.remove();
                 startBtn.remove();
                 finishBtn.remove();
 
-                completeSection.append(div);
+                completeSection.appendChild(div);
 
             });
-            finishBtn.disabled = true;
+
+            descriptionField.value = '';
+            nameField.value = '';
+            phoneField.value = '';
 
             clearBtn[0].addEventListener('click', () => {
-                let orders = Array.from(document.querySelectorAll('#completed-orders .container'));
-                orders.forEach(el => {
-                    el.remove();
-                })
+                document.getElementById('completed-orders').innerHTML = '';
             })
 
         }
