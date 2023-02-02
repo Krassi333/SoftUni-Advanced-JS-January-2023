e51488d6abd93159bb87989a1d00b0ffd8341d54
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-02-02 13:42:58 +0200

    Update the solution

--- JavaScript Advanced/Syntax, Functions and Statements - Exercise/04. Previous Day/Solution/04. Previous Day.js
+++ JavaScript Advanced/Syntax, Functions and Statements - Exercise/04. Previous Day/Solution/04. Previous Day.js
@@ -1,11 +1,11 @@
 function previousDay(year, month, day) {
-    let date;
-    if (day > 1) {
-         date = new Date(year, month, day - 1);
-    } else {
-         date = new Date(year, month - 1, 0);
-    }
-
-    console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
-}
+     let date = new Date(year, month, day - 1);
+     if (day == 1) {
+         date = new Date(year, month - 1, day - 1);
+         console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+     } else {
+         console.log(`${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
+     }
+ 
+ }
 previousDay(2016, 3, 1)
